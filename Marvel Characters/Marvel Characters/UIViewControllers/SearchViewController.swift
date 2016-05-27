54af984689a1cb883f66b55b4abc4ec25c3a1468
Hugo Alonso <hugoalonsoluis@gmail.com>
2016-05-27 12:59:20 -0400

    Improve Search behaviour
    - If empty remove all results and avoid repeating call
    - Always refresh when retrieving results for a new name

--- Marvel Characters/Marvel Characters/UIViewControllers/SearchViewController.swift
+++ Marvel Characters/Marvel Characters/UIViewControllers/SearchViewController.swift
@@ -18,12 +18,20 @@ class SearchViewController: CharacterListViewController {
     var rx_characterName: Observable<String> {
         return searchBar
             .rx_text
-            .filter { !$0.isEmpty } // notice the filter new line
             .throttle(0.5, scheduler: MainScheduler.instance)
+            .filter {
+                if $0.isEmpty {
+                    self.dataSource.value.removeAll()
+                    return false
+                }
+                return true
+            }
             .distinctUntilChanged()
             .doOnNext { (_) in
+                
+                self.dataSource.value.removeAll()
                 self.currentPage.value = 0
-            }
+        }
     }
     
     override func createCharacterService() {
@@ -43,12 +51,12 @@ class SearchViewController: CharacterListViewController {
                 self?.navigationController?.popViewControllerAnimated(true)
             }.addDisposableTo(disposeBag)
         /*
-        searchBar
-            .rx_searchButtonClicked
-            .asDriver()
-            .driveNext { [weak self] (_) in
-                self?.searchBar.endEditing(true)
-            }.addDisposableTo(disposeBag)*/
+         searchBar
+         .rx_searchButtonClicked
+         .asDriver()
+         .driveNext { [weak self] (_) in
+         self?.searchBar.endEditing(true)
+         }.addDisposableTo(disposeBag)*/
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
