3bea0e4386dbf3bc468f3a5acf21f74d85d2a1d8
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2016-05-31 02:28:32 -0400

    Add CrossReference model & json mockup data

---
+++ Marvel Characters/Marvel Characters/Models/CrossReference.swift
@@ -0,0 +1,30 @@
+//
+//  CrossReference.swift
+//  Marvel Characters
+//
+//  Created by Hugo Alonso on 5/31/16.
+//  Copyright © 2016 halonsoluis. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class CrossReference: Mappable {
+    
+    var id: Int?
+    var modified: String?
+    var thumbnail: Thumbnail?
+    var resourceURI: String?
+    
+    
+    //MARK: Mappable protocol
+    required init?(_ map: Map) { }
+    
+    func mapping(map: Map) {
+        
+        id <- map["id"]
+        modified <- map["modified"]
+        thumbnail <- map["thumbnail"]
+        resourceURI <- map["resourceURI"]
+   }
+}
