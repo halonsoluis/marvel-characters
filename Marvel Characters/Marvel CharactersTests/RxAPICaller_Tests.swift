9cce3e5361946a96e9f910cdcb65b64e1d60b73c
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2016-06-04 22:06:08 -0400

    Test suite for models and mockup data loading

---
+++ Marvel Characters/Marvel CharactersTests/RxAPICaller_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  RxAPICaller_Tests.swift
+//  Marvel Characters
+//
+//  Created by Hugo Alonso on 6/4/16.
+//  Copyright © 2016 halonsoluis. All rights reserved.
+//
+
+import XCTest
+
+class RxAPICaller_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
