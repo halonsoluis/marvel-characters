194300a3695fdb578a4b0c79f2e011630f1f4645
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2016-06-04 22:47:32 -0400

    Add Unit Tests to RxAPICaller

--- Marvel Characters/Marvel CharactersTests/RxAPICaller_Tests.swift
+++ Marvel Characters/Marvel CharactersTests/RxAPICaller_Tests.swift
@@ -7,29 +7,114 @@
 //
 
 import XCTest
+import RxSwift
+import Result
+import RxCocoa
+import ObjectMapper
+
+@testable import Marvel_Characters
 
 class RxAPICaller_Tests: XCTestCase {
+    
+    let disposeBag = DisposeBag()
+  
+    let params = ["":""] //Params are ignored when in mockup mode
+    let characterID = 123 //This is ignored when in mockup mode
+    
+    
+    var characterListObservable : Observable<Result<[MarvelCharacter],RequestError>>!
+    var crossReferenceListObservable : Observable<Result<[CrossReference],RequestError>>!
+    
+    let errorValidationMarvelCharacter = { (result: Result<[MarvelCharacter],RequestError>) -> Driver<[MarvelCharacter]> in
+        switch result {
+        case .Success(let character):
+            return Driver.just(character)
+        case .Failure(let error):
+            XCTAssert(false)
+            return Driver.empty()
+        }
+    }
+    
+    let errorValidationCrossReference = { (result: Result<[CrossReference],RequestError>) -> Driver<[CrossReference]> in
+        switch result {
+        case .Success(let character):
+            return Driver.just(character)
+        case .Failure(let error):
+            XCTAssert(false)
+            return Driver.empty()
+        }
+    }
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockupEnabled = true
+        
+        characterListObservable = RxAPICaller.requestWithParams(params, route: Routes.ListCharacters)
+        
+        crossReferenceListObservable = RxAPICaller.requestWithParams(params, route: Routes.ListComicsByCharacter(characterID: characterID))
+        
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testRequestCharacterListObservable() {
+        XCTAssertNotNil(characterListObservable)
+        
+        characterListObservable
+            .flatMapLatest(errorValidationMarvelCharacter)
+            .subscribeNext { (characters) in
+                XCTAssertNotNil(characters)
+                XCTAssertFalse(characters.isEmpty)
+                let first = characters.first
+                XCTAssertNotNil(first)
+                
+                XCTAssertNotNil(first?.name)
+            }.addDisposableTo(disposeBag)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRequestCrossReferenceListObservable() {
+        XCTAssertNotNil(crossReferenceListObservable)
+        
+        crossReferenceListObservable
+            .flatMapLatest(errorValidationCrossReference)
+            .subscribeNext { (characters) in
+                XCTAssertNotNil(characters)
+                XCTAssertFalse(characters.isEmpty)
+                XCTAssertNotNil(characters.first?.title)
+            }.addDisposableTo(disposeBag)
     }
     
-    func testPerformanceExample() {
+    func testPerformanceRequestCrossReferenceListObservable() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+            self.crossReferenceListObservable
+                .flatMapLatest(self.errorValidationCrossReference)
+                .subscribeNext { (_) in
+                }.addDisposableTo(self.disposeBag)
         }
     }
     
+    func testPerformanceRequestCharacterListObservableCreation() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            self.characterListObservable = RxAPICaller.requestWithParams(self.params, route: Routes.ListCharacters)
+        }
+    }
+    func testPerformanceRequestCrossReferenceListObservableCreation() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            self.crossReferenceListObservable = RxAPICaller.requestWithParams(self.params, route: Routes.ListComicsByCharacter(characterID: 123))
+        }
+    }
+    
+    func testPerformanceRequestCharacterListObservable() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+            self.characterListObservable
+                .flatMapLatest(self.errorValidationMarvelCharacter)
+                .subscribeNext { (_) in
+                }.addDisposableTo(self.disposeBag)
+        }
+    }
 }
